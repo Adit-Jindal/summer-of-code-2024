5700d17754dcd169bae04d7c745d5d92243b4812
Adit-Jindal <148064749+Adit-Jindal@users.noreply.github.com>
2024-07-22 18:50:01 +0530

    Add files via upload

---
+++ backend/week-1/App/routes.py
@@ -0,0 +1,12 @@
+from App import app, get_db_connection
+from flask import render_template, request, redirect, url_for
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    conn.close()
+    return "Hello again!"
+@app.route('/hello')
+def hello():
+    return "Hello World!"
